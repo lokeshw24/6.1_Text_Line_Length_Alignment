0ef3bb68246c4878d640ff6460275940c775fa3f
Lokesh Walase <flokeshw24@gmail.com>
2013-02-26 11:07:37 +0530

    fseek() not working as per expectation.In original alignment.c, (temp+1) seems a gamble.!

--- 6.1.4.c
+++ 6.1.4.c
@@ -11,6 +11,7 @@
 
 
 #define ALIGNMENT_LENGTH 31
+#define GAP 10
 #define TRUE 1
 #define FALSE 0
 
@@ -40,22 +41,31 @@ int main(){
 	FILE *new_fp=fopen("formatted_output_file.c" ,"w" );
 
 	while(1){
-		for( ; i<t && no_of_characters && ( *(aligned_line+i)=(char)fgetc(fp) ) != EOF ; i++ , --no_of_characters );
+		for( ; i<t && no_of_characters && ( *(aligned_line+i)=(char)fgetc(fp) ) != EOF ; i++ , --no_of_characters ){
+			//printf("%c(%d) \n", *(aligned_line+i) , no_of_characters );
+
+		}
 			/*if( *(aligned_line+i)=='\n' ) {
 				*(aligned_line+i)=(char)fgetc(fp);
 			}
 			*/
-		printf("%d \n", no_of_characters );
-
 		char *temp=aligned_line+(i-1);
+		printf("Stopped at : %c ", *temp );
 		/*basically, we are supposed to put '\0' at temp,
 		 * but before doing so, lets check if temp has read half word 
 		 * or something like that.
 		 *
 		 */
-		if( *(temp+1)==EOF || no_of_characters<1 ){
+
+
+		/*Changed (temp+1) to (temp) from original code */
+
+		if( *(temp)==EOF || no_of_characters<1 ){
+			printf("Temp , i : %c %d \n", *(temp) , i );
 			*(temp+1)='\0';
 			fprintf( new_fp , "%s\n", aligned_line );
+			fclose(new_fp);
+			//fprintf( new_fp , "%c", EOF );
 			printf("*** \n");
 			break ;
 		}
@@ -76,7 +86,65 @@ int main(){
 
 		fprintf( new_fp , "%s\n", aligned_line );
 
-		//printf("%s$$\n", aligned_line, i , EOF_flag );
+		//printf("%s$$\n", aligned_line  );
+
+		/*now fill the aligned_line, by broken_word, but in reverse order */
+		int k=0;
+		while(i>-1){
+			*(aligned_line+k)=*(broken_word+i);
+			k++;
+			i--;
+		}
+		i=k;
+		//printf("i set to : %d , broken word : %s\n" , i , broken_word);
+
+	}
+
+
+	/*now half of file is being written as 1st column, so 2nd column will be written using fseek() 
+	 */
+	printf("%d \n", no_of_characters);
+	new_fp=fopen("formatted_output_file.c" ,"w" );
+	i=0;
+
+	int sec_col_offset=ALIGNMENT_LENGTH + GAP;
+
+	while(1){
+		for( ; i<t && ( *(aligned_line+i)=(char)fgetc(fp) ) != EOF ; i++ );
+
+		char *temp=aligned_line+(i-1);
+		/*basically, we are supposed to put '\0' at temp,
+		 * * but before doing so, lets check if temp has read half word 
+		 * * or something like that.
+		 * *
+		 * */
+		if( *(temp)==EOF ){
+			*(temp+1)='\0';
+			fseek(new_fp , sec_col_offset , SEEK_CUR );
+			fprintf( new_fp , "%s\n", aligned_line );
+			fclose(new_fp);
+			printf("*** \n");
+			break ;
+
+		}
+
+		else if( *temp==' ' || *temp=='\t' ){
+			broken_word[0]=*temp;
+			*temp='\0';
+			i=0;
+		}
+		else{
+			for(i=0 ; !isspace(*temp) ; i++ ){
+				*(broken_word+i)=*temp;
+				temp--;
+			}
+			*(temp+1)='\0' ;
+			--i;
+		}
+
+		fseek(new_fp , sec_col_offset , SEEK_CUR );
+		fprintf( new_fp , "%s\n", aligned_line );
+		//printf("%s$$\n", aligned_line);
 
 		/*now fill the aligned_line, by broken_word, but in reverse order */
 		int k=0;
@@ -89,8 +157,8 @@ int main(){
 
 	}
 
-	fclose(fp);
 	fclose(new_fp);
+	fclose(fp);
 
 return 0;
 
